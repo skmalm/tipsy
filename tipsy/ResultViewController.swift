26551b4d0e8863f6bd49b7c077cb1fee9498bfe0
Sebastian Malm <skmalm@gmail.com>
2020-02-18 14:06:01 -0500

    Feat. Design result view UI and hook up modal presentation from Calculate button

---
+++ tipsy/ResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResultViewController.swift
+//  tipsy
+//
+//  Created by Sebastian Malm on 2/18/20.
+//  Copyright © 2020 SebastianMalm. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    
+
+}
